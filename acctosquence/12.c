2d046184142aa8b7163b21045efd0c4a85099ea0
Hardik Kumar Sinha <hardikkumarsinha@gmail.com>
2024-12-16 19:46:06 +0530

    dfa

---
+++ acctosquence/12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main() {
+    float a, b, c;
+
+    printf("Enter value of A: ");
+    scanf("%f", &a);
+
+    printf("Enter value of B: ");
+    scanf("%f", &b);
+
+    printf("Enter value of C: ");
+    scanf("%f", &c);
+
+    if ((a < b) && (a < c)) {
+        printf("\nA is the smallest\n");
+
+    } else if ((b < c) && (b < a)) {
+        printf("\nB is the smallest\n");
+
+    } else if ((c < b) && (c < a)) {
+        printf("\nC is the smallest\n");
+
+    } else {
+        printf("invalid\n");
+    }
+}
