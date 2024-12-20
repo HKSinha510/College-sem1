ca6d45ec93813af279484cdd28a992a5b64e9e8b
Hardik Kumar Sinha <hardikkumarsinha@gmail.com>
2024-12-20 23:48:15 +0530

    sach me

--- acctosquence/3.c
+++ acctosquence/3.c
@@ -5,6 +5,8 @@
 void main(){
     printf("My CV: \n");
     printf("\n\n\n");
+    printf("My name is Hardik");
+    printf("I can code in python & C");
+    printf("DOB: 20/01/2005");
     printf("___ END OF CV ___");
-
 }
